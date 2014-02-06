49a1da30b212b6face063f07cb59da8e6076fe59
Martin Smith <martin@mbs3.org>
2014-02-06 10:44:08 -0600

    clone without key, build formula links

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,6 +8,8 @@ yum -y install salt-master git
 git config --global user.name "Martin Smith"
 git config --global user.email "martin@mbs3.org"
 mkdir -p /srv
-cd /srv && git clone git@github.com:martinb3/salt-repo.git
+cd /srv && git clone https://github.com/martinb3/salt-repo.git
 ln -s /srv/salt-repo/salt /srv/salt
 ln -s /srv/salt-repo/pillar /srv/pillar
+
+for f in $(ls /srv/salt-repo/formulas); do ln -s /srv/salt-repo/formulas/$f/$f /srv/salt-repo/salt/$f; done
